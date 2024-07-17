9c19be4465a27413f75a78a83d26b24c9c59c04b
= <=>
2024-07-17 12:13:37 -0500

    first commit

---
+++ bookio/src/main/java/com/rmlee/bookio/BookioApiApplication.java
@@ -0,0 +1,13 @@
+package com.rmlee.bookio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookioApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookioApiApplication.class, args);
+	}
+
+}
---
+++ bookio/src/test/java/com/rmlee/bookio/BookioApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rmlee.bookio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookioApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
